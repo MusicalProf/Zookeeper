3518778143e6dd27856473901affed7ab6eef576
MusicalProf <krishon2006@gmail.com>
2022-02-03 16:11:40 -0600

    Added a Bat class.

---
+++ src/Bat.java
@@ -0,0 +1,19 @@
+public class Bat extends Mammal {
+  public Bat(int energyLevel) {
+    super(energyLevel);
+  }
+
+  public void fly() {
+    System.out.println("WHOOSH!");
+    energyLevel -= 50;
+  }
+
+  public void eatHumans() {
+    System.out.println("YIKES! It's Attacking and eating people!!");
+    energyLevel += 25;
+  }
+
+  public void attackTown() {
+    System.out.println("CRACKLING");
+  }
+}
